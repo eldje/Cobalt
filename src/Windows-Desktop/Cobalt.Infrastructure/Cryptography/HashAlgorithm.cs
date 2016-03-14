7b6f7e67304c7d208ffd0c6018862668e184a824
Travis <captainboatman@outlook.com>
2016-03-13 19:21:08 -0700

    Add initial windows desktop files
    Includes cryptography, email, and account logic and services.

---
+++ src/Windows-Desktop/Cobalt.Infrastructure/Cryptography/HashAlgorithm.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace Cobalt.Infrastructure.Cryptography
+{
+    public static class HashAlgorithm
+    {
+        /// <summary>
+        /// Generates a new hash and salt key. Returns as an array, [0] Hash, [1] Salt.
+        /// </summary>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        public static string[] GenerateNewHash(string password)
+        {
+            var rfc2898DeriveBytes = new Rfc2898DeriveBytes(password, 32)
+            {
+                IterationCount = 10000
+            };
+
+            var hash = rfc2898DeriveBytes.GetBytes(20);
+            var salt = rfc2898DeriveBytes.Salt;
+
+            return new[]
+            {
+                Convert.ToBase64String(hash),
+                Convert.ToBase64String(salt)
+            };
+        }
+
+        /// <summary>
+        /// Returns an existing hash if provided with an existing salt key.
+        /// </summary>
+        /// <param name="password"></param>
+        /// <param name="salt"></param>
+        /// <returns></returns>
+        public static string GenerateExistingHash(string password, byte[] salt)
+        {
+            var rfc2898DeriveBytes = new Rfc2898DeriveBytes(password, salt)
+            {
+                IterationCount = 10000
+            };
+
+            var hash = rfc2898DeriveBytes.GetBytes(20);
+
+            return Convert.ToBase64String(hash);
+        }
+    }
+}
---
+++ src/Windows-Desktop/Cobalt.Infrastructure/Email.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using SendGrid;
+using System.Net.Mail;
+
+namespace Cobalt.Infrastructure
+{
+    public class Email
+    {
+        /// <summary>
+        /// Send a new custom email.
+        /// </summary>
+        /// <param name="from"></param>
+        /// <param name="to"></param>
+        /// <param name="subject"></param>
+        /// <param name="text"></param>
+        public void SendEmail(string from, IEnumerable<string> to, string subject, string text)
+        {
+            try
+            {
+                var newMessage = new SendGridMessage
+                {
+                    From = new MailAddress(from),
+                    Subject = subject,
+                    Text = text
+                };
+
+                newMessage.AddTo(to);
+
+                var transportWeb = new Web(Keys.SendGrid);
+                transportWeb.DeliverAsync(newMessage).Wait();
+            }
+            catch (Exception)
+            {
+                // ignored
+            }
+        }
+    }
+}
