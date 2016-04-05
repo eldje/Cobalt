0b597a3451b9f27c2b594d7ab81a91586514be97
Travis Boatman <captainboatman@outlook.com>
2016-04-04 19:55:53 -0700

    Add Shell Functionality

---
+++ src/Windows-Desktop/Cobalt.Shell/Bootstrapper.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+using Microsoft.Practices.Unity;
+using Prism.Modularity;
+using Prism.Mvvm;
+using Prism.Unity;
+
+namespace Cobalt.Shell
+{
+    public class Bootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return Container.Resolve<Views.MainWindow>();
+        }
+
+        protected override void InitializeShell()
+        {
+            Application.Current.MainWindow.Show();
+        }
+
+        protected override void ConfigureModuleCatalog()
+        {
+            var catalog = (ModuleCatalog)ModuleCatalog;
+        }
+
+        protected override void ConfigureContainer()
+        {
+            base.ConfigureContainer();
+            ViewModelLocationProvider.SetDefaultViewModelFactory(type => Container.Resolve(type));
+        }
+    }
+}
