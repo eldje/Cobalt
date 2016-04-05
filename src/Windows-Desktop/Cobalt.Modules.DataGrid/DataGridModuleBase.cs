82eb441cc177cec67da3ba7216383ed7c1bc96cf
Travis Boatman <captainboatman@outlook.com>
2016-04-04 20:01:29 -0700

    Add New Modules, Remove Testing Project
    Added
    - Ribbon Module
    - DataGrid Module
    
    Removed
    - Accounts Unit Tests

---
+++ src/Windows-Desktop/Cobalt.Modules.DataGrid/DataGridModuleBase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cobalt.Modules.DataGrid.Views;
+using Prism.Modularity;
+using Prism.Regions;
+
+namespace Cobalt.Modules.DataGrid
+{
+    public class DataGridModuleBase : IModule
+    {
+        private readonly IRegionManager _regionManager;
+
+        public DataGridModuleBase(IRegionManager regionManager)
+        {
+            _regionManager = regionManager;
+        }
+
+        public void Initialize()
+        {
+            _regionManager.AddToRegion("DataRegion", new DataGridView());
+        }
+    }
+}
---
+++ src/Windows-Desktop/Cobalt.Modules.Ribbon/RibbonModuleBase.cs
@@ -0,0 +1,22 @@
+﻿using Cobalt.Modules.Ribbon.Views;
+using Microsoft.Practices.Unity;
+using Prism.Modularity;
+using Prism.Regions;
+
+namespace Cobalt.Modules.Ribbon
+{
+    public class RibbonModuleBase : IModule
+    {
+        private readonly IRegionManager _regionManager;
+
+        public RibbonModuleBase(IRegionManager regionManager)
+        {
+            _regionManager = regionManager;
+        }
+
+        public void Initialize()
+        {
+            _regionManager.AddToRegion("RibbonRegion", new RibbonView());
+        }
+    }
+}
